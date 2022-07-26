7e32767cea858c108ac363e1228d08778e3f8424
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-07-26 22:38:15 +0200

    fix slabs not working properly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog].
 
+## [v4.0.1-1.19] - 2022-07-26
+### Fixed
+- Fixed speed boost being applied very unreliably when walking on slab blocks
+
 ## [v4.0.0-1.19] - 2022-07-06
 - Ported to Minecraft 1.19
 - Split into multi-loader project
--- Common/src/main/java/fuzs/blockrunner/mixin/LivingEntityMixin.java
+++ Common/src/main/java/fuzs/blockrunner/mixin/LivingEntityMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -29,13 +30,13 @@ public abstract class LivingEntityMixin extends Entity {
     @Inject(method = "checkFallDamage", at = @At("HEAD"))
     protected void checkFallDamage$head(double y, boolean onGroundIn, BlockState state, BlockPos pos, CallbackInfo callback) {
         if (!this.level.isClientSide && onGroundIn && this.fallDistance > 0.0F) {
-            this.removeCustomBlockSpeed();
-            this.tryAddCustomBlockSpeed();
+            this.custom$removeCustomBlockSpeed();
+            this.custom$tryAddCustomBlockSpeed();
         }
     }
 
     @Unique
-    protected void removeCustomBlockSpeed() {
+    protected void custom$removeCustomBlockSpeed() {
         AttributeInstance attributeinstance = this.getAttribute(Attributes.MOVEMENT_SPEED);
         if (attributeinstance != null) {
             if (attributeinstance.getModifier(BlockSpeedManager.SPEED_MODIFIER_CUSTOM_BLOCK_SPEED_UUID) != null) {
@@ -45,10 +46,10 @@ public abstract class LivingEntityMixin extends Entity {
     }
 
     @Unique
-    protected void tryAddCustomBlockSpeed() {
+    protected void custom$tryAddCustomBlockSpeed() {
         if (!this.getBlockStateOn().isAir()) {
-            if (this.onCustomSpeedBlock()) {
-                double customSpeed = BlockSpeedManager.INSTANCE.getSpeedFactor(this.level.getBlockState(this.getBlockPosBelowThatAffectsMyMovement()).getBlock());
+            if ((!((Object) this instanceof Player player) || !player.getAbilities().flying) && this.custom$onCustomSpeedBlock()) {
+                double customSpeed = BlockSpeedManager.INSTANCE.getSpeedFactor(this.level.getBlockState(this.custom$getBlockPosBelowThatAffectsMyMovement2()).getBlock());
                 AttributeInstance attributeinstance = this.getAttribute(Attributes.MOVEMENT_SPEED);
                 if (attributeinstance == null || customSpeed == 1.0) {
                     return;
@@ -64,22 +65,28 @@ public abstract class LivingEntityMixin extends Entity {
     public abstract AttributeInstance getAttribute(Attribute p_21052_);
 
     @Unique
-    protected boolean onCustomSpeedBlock() {
-        return BlockSpeedManager.INSTANCE.hasCustomSpeed(this.level.getBlockState(this.getBlockPosBelowThatAffectsMyMovement()).getBlock());
+    protected boolean custom$onCustomSpeedBlock() {
+        return BlockSpeedManager.INSTANCE.hasCustomSpeed(this.level.getBlockState(this.custom$getBlockPosBelowThatAffectsMyMovement2()).getBlock());
+    }
+
+    @Unique
+    protected BlockPos custom$getBlockPosBelowThatAffectsMyMovement2() {
+        // reduce 0.5000001 (8/16) to 0.4375001 (7/16) to allow slabs to work properly
+        return new BlockPos(this.getX(), this.getBoundingBox().minY - 0.4375001, this.getZ());
     }
 
     @Inject(method = "getBlockSpeedFactor", at = @At("HEAD"), cancellable = true)
     protected void getBlockSpeedFactor$head(CallbackInfoReturnable<Float> callback) {
-        if (this.onCustomSpeedBlock()) callback.setReturnValue(1.0F);
+        if (this.custom$onCustomSpeedBlock()) callback.setReturnValue(1.0F);
     }
 
     @Inject(method = "onChangedBlock", at = @At("TAIL"))
     protected void onChangedBlock$tail(BlockPos pos, CallbackInfo callback) {
         // check if block not air or player is elytra flying
         if (this.shouldRemoveSoulSpeed(this.getBlockStateOn())) {
-            this.removeCustomBlockSpeed();
+            this.custom$removeCustomBlockSpeed();
         }
-        this.tryAddCustomBlockSpeed();
+        this.custom$tryAddCustomBlockSpeed();
     }
 
     @Shadow
